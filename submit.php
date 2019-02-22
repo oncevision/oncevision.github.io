0d7d83709802a23f06c90646542cfdff364c7891
oncevision <github@oncevision.com>
2019-02-22 10:12:56 -0700

    Delete submit.php

--- submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-$response=$_POST["response"];
-$url="https://www.google.com/recaptcha/api/siteverify?secret=".$secret"&response=".$response;
-$verify=file_get_contents(Surl);
-echo $verify;
-?>
